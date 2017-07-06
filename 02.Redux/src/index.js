d07e12a80ba9ab8144c86acd70eb877cb37b351c
Krzysztof Trzewiczek <trzewiczek@protonmail.ch>
2017-07-06 06:38:56 +0200

    Ch02 | Doc strings

--- 02.Redux/src/index.js
+++ 02.Redux/src/index.js
@@ -1,3 +1,13 @@
+/**
+ * A Todo reducer.
+ *
+ * Supported action types:
+ *  - ADD_TODO
+ *
+ * @param {Object} [state = { todos: [] }] Current state of the store
+ * @param {Object} action Dispatched action
+ * @return {Object} New state if the store
+ */
 export const reducer = (state = { todos: [] }, action) => {
   switch (action.type) {
     case 'ADD_TODO':
